888130c990ff39947f3e9eaf2221d19fc8a0da12
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-11 17:37:18 +0200

    secondo esercizio completato

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -18,17 +18,17 @@ function vipVerifier() {
     for (let i = 0; i < vipsList.length; i++) {
         if (userMail === vipsList[i]) {
             console.log("sei dentro");
-            document.getElementById("in_or_out").innerHTML = "sei dentro !"
+            document.getElementById("in_or_out").innerHTML = "sei dentro !";
         } else {
             console.log("sei fuori");
-            document.getElementById("in_or_out").innerHTML = "sei fuori..."
+            document.getElementById("in_or_out").innerHTML = "sei fuori...";
         }
     }
 }
 
 function displayNone() {
     document.getElementById("result").style.display = "none";
-    const empty = document.getElementById("mail")
+    const empty = document.getElementById("mail");
     empty.value = '';
 }
 
@@ -38,3 +38,22 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. St
 Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
 
 /* tramite un bottone genero un numero casuale da 1 a 6 */
+/* ne genero due in realtà, uno per me e uno per il pc  */
+/* confronta i due risultati, quello più alto vince */
+
+document.getElementById("randomNumber").onclick = function(){
+    let playerResult = Math.floor(Math.random() * 6) + 1;
+    let computerResult = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("player_result").innerHTML = playerResult;
+    document.getElementById("computer_result").innerHTML = computerResult;
+    if (playerResult > computerResult) {
+        console.log("player vince");
+        document.getElementById("theWinnerIs").innerHTML = "You Won !"
+    } else if (playerResult < computerResult) {
+        console.log("computer vince");
+        document.getElementById("theWinnerIs").innerHTML = "You Lost..."
+    } else {
+        console.log("Pareggio");
+        document.getElementById("theWinnerIs").innerHTML = "Draw ! Nobody Wins !"
+    }    
+}
